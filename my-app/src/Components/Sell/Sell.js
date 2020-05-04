ca1e15321b3ff939b92ff934d37ff4264ddd30ba
1nsideR-1337 <eln.ismayilov@gmail.com>
2020-05-04 00:12:11 -0700

    'designrevoke'

--- my-app/src/Components/Sell/Sell.js
+++ my-app/src/Components/Sell/Sell.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { Component } from 'react';
+import './Sell.css';
 
-class Sell extends React.Component{
+
+class Sell extends Component {
+
+    
+
     render() {
+       
         return (
-            <div>
-                Sell
+            <div className="main">
+
+                <div className="container">
+                    <div><i className="glyphicon glyphicon-menu-left"></i><button onClick={() => this.props.history.goBack()}>Back</button></div>
+                    <div><p className="stock-name">Stock's Name</p></div>
+                </div>
+                <div className="buy-defaut-price"><p>Default Price</p></div>
+                <div className="buy-quantity">
+                    <button onClick={this.handleMinus}>-</button>
+                    <span>X</span>
+                    <button onClick={this.handlePlus}>+</button>
+                </div>
+                <div className="buy-price">
+                    <span>Sell for </span>
+                    <span>totalPrice</span>
+                    <span>$</span>
+                </div>
+                <div className="sell-button" > <button onClick={this.handleBuy}>Sell</button></div>
+               
             </div>
-        );
+        )
     }
 }
 
